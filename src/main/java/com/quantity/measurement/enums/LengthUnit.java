7f87d6d6efb04f738f4f6c60b13265cfbf5c10bb
Nishant-Rana1 <nishant7163.ca22@chitkara.edu.in>
2026-04-01 13:50:30 +0530

    UC4

--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -1,16 +1,19 @@
 package com.quantity.measurement.enums;
 
 public enum LengthUnit {
-    FEET(1.0),
-    INCH(1.0 / 12);
 
-    private final double toFeetFactor;
+    INCH(1.0),              // base unit
+    FEET(12.0),             // 1 foot = 12 inches
+    YARDS(36.0),            // 1 yard = 36 inches
+    CENTIMETERS(0.393701);  // 1 cm = 0.393701 inches
 
-    LengthUnit(double toFeetFactor  ) {
-        this.toFeetFactor = toFeetFactor;
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
     }
 
-    public double toFeet(double value) {
-        return value * toFeetFactor;
+    public double toBase(double value) {
+        return value * conversionFactor;
     }
 }
--- src/main/java/com/quantity/measurement/model/QuantityLength.java
+++ src/main/java/com/quantity/measurement/model/QuantityLength.java
@@ -7,24 +7,26 @@ public class QuantityLength {
     private final double value;
     private final LengthUnit unit;
 
-
     public QuantityLength(double value, LengthUnit unit) {
-        if(unit == null) throw new IllegalArgumentException("unit can not null ");
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null");
+        }
         this.value = value;
         this.unit = unit;
     }
+
     @Override
     public boolean equals(Object obj) {
 
         if (this == obj) return true;
 
         if (obj == null || getClass() != obj.getClass()) return false;
+
         QuantityLength other = (QuantityLength) obj;
 
-        double thisInFeet = this.unit.toFeet(this.value);
-        double otherInFeet = this.unit.toFeet(this.value);
+        double thisValue = this.unit.toBase(this.value);
+        double otherValue = other.unit.toBase(other.value);
 
-
-        return Double.compare(thisInFeet,otherInFeet)==0;
+        return Double.compare(thisValue, otherValue) == 0;
     }
 }
